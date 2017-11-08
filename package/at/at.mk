8cde3e3cfe970db77801323301619226cd688f02
lixc <xichao@sina.cn>
2017-11-08 12:36:02 +0800

    1.修改S99at,创建所需的文件 2.增加atd.pam 3.修改at.mk,编译时不定义SENDMIAL,安装时拷贝atd.pam

--- package/at/at.mk
+++ package/at/at.mk
@@ -18,8 +18,13 @@ AT_CONF_OPTS = \
 	--with-jobdir=/var/spool/cron/atjobs \
 	--with-atspool=/var/spool/cron/atspool \
 	--with-daemon_username=root \
-	--with-daemon_groupname=root \
-	SENDMAIL=/usr/sbin/sendmail
+	--with-daemon_groupname=root
+
+# Install default pam config 
+define AT_INSTALL_CONFIG
+        $(INSTALL) -m 0644 -D package/at/atd.pam \
+                $(TARGET_DIR)/etc/pam.d/atd
+endef
 
 define AT_INSTALL_INIT_SYSV
 	$(INSTALL) -m 0755 -D package/at/S99at $(TARGET_DIR)/etc/init.d/S99at
